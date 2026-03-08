4ee980048c8760248c084dd865fddf137cb48364
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2026-03-08 16:24:50 +0000

    fix: relolve build task issue (remove unused hello message emission from UI bootstrap)

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,4 +1,4 @@
-import { PLUGIN, UI } from "@common/networkSides";
+import { UI } from "@common/networkSides";
 import { UI_CHANNEL } from "@ui/app.network";
 import { Networker } from "monorepo-networker";
 import { render } from "preact";
@@ -7,8 +7,6 @@ import "./styles.css";
 
 async function bootstrap() {
   Networker.initialize(UI, UI_CHANNEL);
-
-  UI_CHANNEL.emit(PLUGIN, "hello", ["Hey there, Figma!"]);
 
   const App = (await import("./app")).default;
 
